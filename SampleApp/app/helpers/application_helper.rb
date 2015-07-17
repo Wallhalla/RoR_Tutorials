c90f856687744e1f377634dfbb746bdc191534e7
Wallhalla <f.sonczeck@ostfalia.de>
2015-07-17 14:29:24 +0000

    full_title helper to fit doc title without a specific title

--- SampleApp/app/helpers/application_helper.rb
+++ SampleApp/app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  
+  #returns full title on a per page basis
+  def full_title(page_title = '')
+    
+    base_title = "Ruby on Rails Tutorial SampleApp"
+    
+    if page_title.empty?
+      return base_title
+    else
+      return "#{page_title} | #{base_title}"
+    end
+  
+  end
 end
