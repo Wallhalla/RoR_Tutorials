600791fac3a6a4a0cc1cb179880f667cfea071f7
Wallhalla <f.sonczeck@ostfalia.de>
2015-07-18 16:55:04 +0000

    routing to SignUp paSignUp page (users#new) finished

--- SampleApp/test/controllers/users_controller_test.rb
+++ SampleApp/test/controllers/users_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  
   test "should get new" do
     get :new
     assert_response :success
+    assert_select "title", "SignUp | Ruby on Rails Tutorial SampleApp"
   end
 
 end
