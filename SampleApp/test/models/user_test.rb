6e54085b710bccbbed0db2fd88b65428e5bfedc9
Wallhalla <f.sonczeck@ostfalia.de>
2015-07-19 09:33:09 +0000

    validation test for user name, name cant be blank

--- SampleApp/test/models/user_test.rb
+++ SampleApp/test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @valid_user = User.new(name: "TestUser", email: "TestUser@example.com")
+  end
+  
+  test "should be valid" do
+    assert @valid_user.valid?
+  end
+  
+  test "name should be present" do
+    @valid_user.name = "  "
+    assert_not @valid_user.valid?
+  end
+  
 end
