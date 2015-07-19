f46db228713acc0ad048ad97dcf1355e5dad0500
Wallhalla <f.sonczeck@ostfalia.de>
2015-07-19 10:10:16 +0000

    length restriction to name and email added

--- SampleApp/test/models/user_test.rb
+++ SampleApp/test/models/user_test.rb
@@ -15,4 +15,24 @@ class UserTest < ActiveSupport::TestCase
     assert_not @valid_user.valid?
   end
   
+  test "email should be present" do
+    @valid_user.email = "  "
+    assert_not @valid_user.valid?
+  end
+  
+  test "name should not be too long" do
+    @valid_user.name = "a" * 51
+    assert_not @valid_user.valid?
+  end
+  
+  test "email should not be too long" do
+    sDomain = @valid_user.email.split('@')[1]
+    @valid_user.email = "a" * 255 + sDomain
+    assert_not @valid_user.valid?
+  end
+  
+  test "email should be a valid email format" do
+    
+  end
+  
 end
