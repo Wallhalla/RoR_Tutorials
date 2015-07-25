6f6e7e8a2df55372452be8d5d34c3f2c8a07fcbe
Wallhalla <f.sonczeck@ostfalia.de>
2015-07-25 07:10:19 +0000

    secure password for users declared in model, password_digest migration generated

--- SampleApp/app/models/user.rb
+++ SampleApp/app/models/user.rb
@@ -16,5 +16,7 @@ class User < ActiveRecord::Base
             format: {with: VALID_EMAIL_REGEX},
             uniqueness: {case_sensitive: false}
             )
+
+has_secure_password
             
 end
